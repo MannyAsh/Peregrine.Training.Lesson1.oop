4ada5c7538a93217030da22262df2bd1d66e5ad1
MannyAsh <amaan.ashiq.aa@gmail.com>
2023-09-14 12:31:02 +0100

    Completed

---
+++ Inheritance/Inheritance/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+     class Account : Person {
+
+        public Account() { }
+        public double getAccountBalance() {
+
+            return currentAccount;
+
+        }
+
+        public double setAccountBalance(double Amount) { 
+            currentAccount = currentAccount + Amount;
+            return currentAccount;
+
+        }
+
+        public double withdrawFromAccount(double amountToWithdraw) {
+
+            currentAccount = currentAccount - amountToWithdraw;
+
+            return currentAccount;
+        
+        }
+    }
+}
---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            Account myAccount = new Account();
+
+            double currentAmount = 0.0;
+
+            currentAmount = myAccount.setAccountBalance(1000.00);
+            Console.WriteLine("Current amount: {0}", currentAmount);
+            
+
+            currentAmount = myAccount.withdrawFromAccount(200.00);
+            Console.WriteLine("Current amount: {0}", currentAmount);
+            
+
+            currentAmount = myAccount.getAccountBalance();
+            Console.WriteLine("Current amount: {0}", currentAmount);
+            Console.ReadLine();
+        }
+    }
+}
